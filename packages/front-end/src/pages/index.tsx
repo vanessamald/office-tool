37713edc25d2f6418b35adac9ff508ee8c119016
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-10-25 13:27:51 -0500

    seeding db, displaying all users

--- packages/front-end/src/pages/index.tsx
+++ packages/front-end/src/pages/index.tsx
@@ -1,10 +1,34 @@
 import clsx from 'clsx';
 import Head from 'next/head';
 import { Inter } from '@next/font/google';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  //const router = useRouter();
+
+  // fetch user data
+  const [ users, setUsers ] = useState([]);
+
+  useEffect(() => {
+    async function fetchUserData() {
+      try {
+        const response = await fetch('http://localhost:50000/users')
+        if (response.ok) {
+          const results = await response.json();
+          setUsers(results.data);
+        } else {
+          console.error('Error fetching data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchUserData();
+  }, []);
+
   return (
     <>
       <Head>
@@ -18,6 +42,16 @@ export default function Home() {
         </h1>
         <div className='p-4'>
           <p className='text-neutral-500'>Hello, world.</p>
+          {/* Display ALL USERS, if no users send message 'No users available' */}
+          {users.length > 0 ? (
+            <ul>
+              {users.map((user)=> (
+                <li key={user.id}>{user.firstName} {user.lastName}</li>
+                ))}
+            </ul>
+            ) : (
+            <p>No users available.</p>
+            )}
         </div>
       </main>
     </>
