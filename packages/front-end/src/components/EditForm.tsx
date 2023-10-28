7128d93d6ee1192bea1b00d62d484671d2f35545
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-10-27 19:32:22 -0500

    adding buttons to edit form

--- packages/front-end/src/components/EditForm.tsx
+++ packages/front-end/src/components/EditForm.tsx
@@ -7,7 +7,19 @@ export default function EditForm ({user, onClose})  {
     return (
         <>
             <div>
-                
+                <button
+                    className="text-green-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    Submit
+                </button>
+                <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={onClose}
+                  >
+                    Close
+                  </button>
             </div>
         </>
     )
